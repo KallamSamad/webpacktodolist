294a5e9ce3a537b9926de127b174d5b812915f77
Kallam <kallamsamad@gmail.com>
2026-04-27 16:24:47 +0100

    Added eventlistener to a form to accept inputted values for the to do list

--- src/index.js
+++ src/index.js
@@ -5,33 +5,54 @@ import { Project, ToDo } from "./class.js";
 const myProject = new Project("Uni Work");
 const myTodo = new ToDo("Finish Odin", "Todo app", "2026-04-30", "High", "");
 
-myProject.addtoDo(myTodo);
-console.log(myTodo.title)
+
+
  
 
 const appbody=document.body.querySelector(".app");
 const appHeader=document.createElement("div");
-appHeader.className="derp";
+
 appHeader.innerHTML="<h1>To Do List</h1>"
-appHeader.style.backgroundColor="cyan";
+appHeader.style.backgroundColor="grey";
 appbody.appendChild(appHeader);
 
 
-
+const form = document.querySelector(".toDoForm");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+     
+    const title = form.title.value;
+    const description = form.description.value;
+    const dueDate = form.due.value;
+    const priority = form.priority.value;
+    const notes = form.querySelector("textarea").value;
+    const newTodo = new ToDo(title, description, dueDate, priority, notes);
+    myProject.addtoDo(newTodo);
+    console.log(newTodo); 
+    renderTodos();
  
-
-myProject.todos.forEach(element => {
-    const todoCard = document.createElement("div");
-    todoCard.innerHTML=
-    `<h2>${element.title}</h2>
-    <p>${element.description}</p>
-    <p>${element.dueDate}</p>
-    <p>${element.priority}</p>
-    <p>${element.notes}</p>
-
-    `;
-    appbody.appendChild(todoCard);
-    
 });
 
+const todoList = document.querySelector(".todoList");
+
+function renderTodos() {
+    todoList.innerHTML = ""; // clear old todos
+
+    myProject.todos.forEach(element => {
+        const todoCard = document.createElement("div");
+         
+        todoCard.innerHTML = `
+        <h2>${element.title}</h2>
+        <p>${element.description}</p>
+        <p>${element.dueDate}</p>
+        <p>${element.priority}</p>
+        <p>${element.notes}</p>
+          `;              
+        const cancel=document.createElement("button");
+        cancel.textContent="Delete Note";
+        
+        todoList.appendChild(todoCard);
+        todoList.appendChild(cancel);
+    });
+}
  
